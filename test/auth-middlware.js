076b0b3a9a2c8d3acde54b7fc2e2471b37c01faa
Mona Lisa <yemoelwin142@gmail.com>
2023-05-19 15:28:26 +0630

    testing

---
+++ test/auth-middlware.js
@@ -0,0 +1,46 @@
+const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
+
+const authMiddleware = require('../middleware/is-auth');
+
+describe('Auth middleware', function () {
+
+    it('should throw an error if no authorization header is present', function () {
+        const req = {
+            get: function (headerName) {
+                return null;
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw(
+            'Not authenticated.'
+        );
+    });
+
+    it('should yield a userId after decoding the token', function () {
+        const req = {
+            get: function (headerName) {
+                return 'Bearer dagfjghmukewrtrz';
+            }
+        };
+        sinon.stub(jwt, 'verify');
+        jwt.verify.returns({ userId: 'abc' });
+        authMiddleware(req, {}, () => { });
+        expect(req).to.have.property('userId');
+        expect(req).to.have.property('userId', 'abc');
+        expect(jwt.verify.called).to.be.true;
+        jwt.verify.restore();
+    });
+
+    it('should throw an error if the authorization header is only one string', function () {
+        const req = {
+            get: function (headerName) {
+                return 'xyz';
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw();
+    });
+
+
+})
+
